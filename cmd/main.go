4070a5e5ef4a28f127d1eb53163655bb8891e0b1
Cameron Adams <cadams@fleetio.com>
2024-09-14 01:13:14 -0500

    feat: add social auth for google and tiktok

--- cmd/main.go
+++ cmd/main.go
@@ -1,30 +1,38 @@
 package main
 
 import (
+	config "app"
 	"app/controllers"
 	"app/database"
-  middleware_handlers "app/middleware"
+	middleware_handlers "app/middleware"
+	"context"
+	"fmt"
+	"html/template"
 
 	"net/http"
 
 	"github.com/gorilla/sessions"
 	"github.com/labstack/echo-contrib/session"
-	"github.com/labstack/echo/v4"
-	"github.com/labstack/echo/v4/middleware"
+  "github.com/labstack/echo/v4"
+  "github.com/labstack/echo/v4/middleware"
+  "github.com/markbates/goth"
+  "github.com/markbates/goth/gothic"
+  "github.com/markbates/goth/providers/tiktok"
+  "github.com/markbates/goth/providers/google"
 )
 
 func main() {
 	app := echo.New()
 
-  database.ConnectDB()
+	database.ConnectDB()
 
 	app.Use(middleware.LoggerWithConfig(middleware.LoggerConfig{
 		Format: "${time_rfc3339} ${method} ${uri} ${status}\n",
 	}))
 	app.Use(session.Middleware(sessions.NewCookieStore([]byte("secret"))))
-  app.Use(middleware.CSRFWithConfig(middleware.CSRFConfig{
-    TokenLookup: "form:_csrf",
-  }))
+	app.Use(middleware.CSRFWithConfig(middleware.CSRFConfig{
+		TokenLookup: "form:_csrf",
+	}))
 
 	app.GET("/health", func(c echo.Context) error {
 		return c.String(http.StatusOK, "healthy!")
@@ -32,36 +40,70 @@ func main() {
 
 	app.Static("/assets", "assets")
 
-  application_views_controller := controllers.ApplicationViewHandler{}
-  app.GET("/error", application_views_controller.HandleErrorIndex)
-  app.GET("/not_found", application_views_controller.HandleNotFoundIndex)
-  app.GET("/", application_views_controller.HandleHomeIndex)
+	application_views_controller := controllers.ApplicationViewHandler{}
+	app.GET("/error", application_views_controller.HandleErrorIndex)
+	app.GET("/not_found", application_views_controller.HandleNotFoundIndex)
+	app.GET("/", application_views_controller.HandleHomeIndex)
 
-  authentication_controller := controllers.AuthenticationController {}
-  app.GET("/login", authentication_controller.HandleLoginIndex)
-  app.POST("/auth/login", authentication_controller.HandleLoginCreate)
-  app.GET("/register", authentication_controller.HandleRegisterIndex)
-  app.POST("/auth/register", authentication_controller.HandleRegisterCreate)
-  app.GET("/forgot-password", authentication_controller.HandleForgotPasswordIndex)
-  app.GET("/auth/send-reset-password-email", authentication_controller.HandleSendResetPasswordEmail)
-  app.GET("/logout", authentication_controller.HandleLogout)
+	authentication_controller := controllers.AuthenticationController{}
+	app.GET("/login", authentication_controller.HandleLoginIndex)
+
+	store := sessions.NewCookieStore([]byte("secret"))
+
+	gothic.Store = store
+
+	goth.UseProviders(
+		google.New(config.GetConfig("GOOGLE_CLIENT_ID"), config.GetConfig("GOOGLE_CLIENT_SECRET"), "http://localhost:4000/auth/google/callback"),
+    tiktok.New(config.GetConfig("TIKTOK_CLIENT_ID"), config.GetConfig("TIKTOK_CLIENT_SECRET"), "http://localhost:4000/auth/tiktok/callback"),
+	)
+
+	app.GET("/auth/:provider/callback", func(c echo.Context) error {
+		ctx := context.WithValue(c.Request().Context(), gothic.ProviderParamKey, c.Param("provider"))
+
+		_, err := gothic.CompleteUserAuth(c.Response(), c.Request().WithContext(ctx))
+		if err != nil {
+			return err
+		}
+
+    return c.Redirect(http.StatusPermanentRedirect, "/")
+	})
+
+	app.GET("/logout/:provider", func(c echo.Context) error {
+		ctx := context.WithValue(c.Request().Context(), gothic.ProviderParamKey, c.Param("provider"))
+
+		gothic.Logout(c.Response(), c.Request().WithContext(ctx))
+		return c.Redirect(http.StatusPermanentRedirect, "/")
+	})
+
+	app.GET("/auth/:provider", func(c echo.Context) error {
+		ctx := context.WithValue(c.Request().Context(), gothic.ProviderParamKey, c.Param("provider"))
+
+		// try to get the user without re-authenticating
+		if _, err := gothic.CompleteUserAuth(c.Response(), c.Request().WithContext(ctx)); err == nil {
+      return c.Redirect(http.StatusPermanentRedirect, "/")
+		}
+
+		gothic.BeginAuthHandler(c.Response(), c.Request().WithContext(ctx))
+
+		return nil
+	})
 
 	app.Use(middleware_handlers.SetSessionInContext)
 	app.Use(middleware_handlers.NoSessionRedirect)
 
-  users := app.Group("/users")
-  users_controller := controllers.UsersController {}
-  users.GET("/edit", users_controller.HandleUsersEdit)
-  users.PATCH("/update", users_controller.HandleUsersUpdate)
+	users := app.Group("/users")
+	users_controller := controllers.UsersController{}
+	users.GET("/edit", users_controller.HandleUsersEdit)
+	users.PATCH("/update", users_controller.HandleUsersUpdate)
 
-  subtitles := app.Group("/subtitles")
-  subtitles_controller := controllers.SubtitlesController{}
-  subtitles.POST("/create", subtitles_controller.HandleSubtitlesCreate)
+	subtitles := app.Group("/subtitles")
+	subtitles_controller := controllers.SubtitlesController{}
+	subtitles.POST("/create", subtitles_controller.HandleSubtitlesCreate)
 
-  uploads := app.Group("/uploads")
-  uploads_controller := controllers.UploadsController{}
-  uploads.GET("", uploads_controller.HandleUploadsIndex)
-  uploads.GET("/:id", uploads_controller.HandleUploadsShow)
+	uploads := app.Group("/uploads")
+	uploads_controller := controllers.UploadsController{}
+	uploads.GET("", uploads_controller.HandleUploadsIndex)
+	uploads.GET("/:id", uploads_controller.HandleUploadsShow)
 
 	app.Logger.Fatal(app.Start(":4000"))
 }
